e445296019f763c5b0232f445b2aed3f67de72c2
alexei guevara <alexguev@gmail.com>
2011-04-04 00:56:56 -0400

    playing with google auth

--- src/main/scala/net/securedurl/pages/HomePage.scala
+++ src/main/scala/net/securedurl/pages/HomePage.scala
@@ -2,12 +2,20 @@ package net.securedurl.pages;
 
 import org.apache.wicket.PageParameters
 import org.apache.wicket.markup.html.basic.Label
-import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.WebPage
 import org.apache.wicket.spring.injection.annot.SpringBean
+import com.google.appengine.api.users.UserService
+import com.google.appengine.api.users.UserServiceFactory
 
 class HomePage(parameters: PageParameters) extends WebPage {
-	add(new Label("message", title))
+	add(new Label("message", generateWelcome))
 	
-	@SpringBean(name="title") val title: String = "N/A"	
+	@SpringBean(name="title") val title: String = "N/A"
+		
+	def generateWelcome: String = {
+		val userService = UserServiceFactory.getUserService
+		val user = userService.getCurrentUser
+		return user.getNickname
+	}
 		
 }
